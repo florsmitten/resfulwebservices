df08137ee2a68c8b69354fdfdc986de42ce522f8
florr <florr@Flor_Thinkpad>
2022-07-12 15:14:45 -0300

    Primer push.

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	
+	//GET
+	//URI - /hello-world
+	//method - "Hello World"
+	
+	//@RequestMapping(method=RequestMethod.GET,path="/hello-world")
+	//puedo reemplazarlo por:
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		
+		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		
+		return new HelloWorldBean("Hello World");
+	}
+
+	// /hello-world/path-variable/in28minutes
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		
+		return new HelloWorldBean(String.format("Hello World, %s",name));
+	}
+}
