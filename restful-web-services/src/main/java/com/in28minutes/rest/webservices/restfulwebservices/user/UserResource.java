de439406e86ad4f29c94df8469c9449b235ecdcb
florr <florr@Flor_Thinkpad>
2022-07-13 14:08:19 -0300

    Avances.

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,14 +1,23 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -26,9 +35,23 @@ public class UserResource {
 	
 	//retrieveUser
 	@GetMapping("/users/{id}")
-	public User retrieveUsers(@PathVariable int id){
+	public EntityModel<User> retrieveUsers(@PathVariable int id){
 		
-		return service.findOne(id);
+		User user = service.findOne(id);
+		
+		if(user==null) {
+			
+			throw new UserNotFoundException("id-" + id);
+		}
+		
+		EntityModel<User> model = EntityModel.of(user);
+		
+		//Permite agregar al body del response un link
+		WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		
+		model.add(linkToUsers.withRel("all-users"));
+		
+		return model;
 	}
 	
 	//created
@@ -36,12 +59,22 @@ public class UserResource {
 	//output - created and return the created URI
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		
 		User savedUser = service.save(user);
 		
-		//ServletUri
-		//ResponseEntity<T>.created(null);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id){
 		
+		User user = service.deletById(id);
+		
+		if(user==null) {
+			
+			throw new UserNotFoundException("id-" + id);
+		}
+	}	
 }
